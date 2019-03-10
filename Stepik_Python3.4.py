8a59290abd5593ecde724cf6e079a424e9f6b790
Cweldulw <cweldulw@gmail.com>
2019-03-10 10:15:40 +0300

    Задание 3.4

--- Stepik_Python3.4.py
+++ Stepik_Python3.4.py
@@ -1,11 +1,13 @@
 i=0
-k=''
-s=''
+k=1
+n=''
+m=0
 s1=''
 s2=''
-n=0
-l=''
-str1="a3b4c2e10b1"
+a=[]
+a1=[0, 0]
+
+str1="a3b4c2e10b1c3"
 str2=''
 
 
@@ -14,20 +16,30 @@ str2=''
 #with open('dataset.txt') as inp:
 #    str1 = inp.readline().strip()
 
-while i <= len(str1):
-    s = str1[i]
-    k = str1[i+1]
-    if s.isdigit() and k.isdigit():
-        l=s+k
-        s2=str1[i-1]
-    elif s.isdigit() and not k.isdigit():
-        n = int(l)
-        str2 = str2 + s2*n
+while i < len(str1):
+    s1 = str1[i]
+    if i+1 < len(str1):
+        s2 = str1[i+1]
     else:
-        k = ''
-        s1 = s
-        s2 = s
+        s2=''
+    if not s1.isdigit() and not s2.isdigit():
+        a.append(s1, 1)
+    elif not s1.isdigit() and s2.isdigit():
+        k=i+1
+        while s2.isdigit():
+            n = n+s2
+            if k+1 < len(str1):
+                s2 = str1[k+1]
+            else:
+                s2=''
+            k +=1
+        m = int(n)
+        n = ''
+        a1 = [s1, m]
+        a.append(a1)
+#    elif s1.isdigit():
     i +=1
 
 
-print(str2)
+
+#print(str2)
