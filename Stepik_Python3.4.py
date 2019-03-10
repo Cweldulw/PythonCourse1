f1e280397313a060a188735886fde000fa757e9a
Cweldulw <cweldulw@gmail.com>
2019-03-10 14:47:56 +0300

    Задание 3.4

--- Stepik_Python3.4.py
+++ Stepik_Python3.4.py
@@ -6,15 +6,13 @@ s1=''
 s2=''
 a=[]
 a1=[0, 0]
-
-str1="a3b4c2e10b1c3"
+#str1="a3b4c2e10b1c3"
 str2=''
 
 
+with open('dataset.txt') as inp:
+    str1 = inp.readline().strip()
 
-
-#with open('dataset.txt') as inp:
-#    str1 = inp.readline().strip()
 
 while i < len(str1):
     s1 = str1[i]
@@ -40,6 +38,14 @@ while i < len(str1):
         a.append(a1)
 #    elif s1.isdigit():
     i +=1
+
+str2=''
+
+for i in range(len(a)):
+    str2=str2+a[i][0]*a[i][1]
+
+with open('dataout.txt' 'w') as outp:
+    outp.write(str2)
 
 
 
